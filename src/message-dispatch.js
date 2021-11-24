e2a83cdead01679e1ff317905f006946a91f7149
isseiini <i-kurata@vleap.jp>
2021-11-25 05:24:12 +0900

    2021-11-25-ver16

--- src/message-dispatch.js
+++ src/message-dispatch.js
@@ -2,102 +2,23 @@ import "./utils/configs";
 import { getAbsoluteHref } from "./utils/media-url-utils";
 import { isValidSceneUrl } from "./utils/scene-url-utils";
 import { spawnChatMessage } from "./react-components/chat-message";
-import { SOUND_CHAT_MESSAGE, SOUND_QUACK, SOUND_SPECIAL_QUACK } from "./systems/sound-effects-system";
+import { SOUND_QUACK, SOUND_SPECIAL_QUACK } from "./systems/sound-effects-system";
 import ducky from "./assets/models/DuckyMesh.glb";
 import { EventTarget } from "event-target-shim";
 import { ExitReason } from "./react-components/room/ExitedRoomScreen";
 import { LogMessageType } from "./react-components/room/ChatSidebar";
-import { entries } from "core-js/core/array";
 
 let uiRoot;
-
-var Player_UI = document.getElementById("Player-UI")
-
-const lifeBar = document.getElementById('life-bar')         // ライフバー
-const lifeMark = document.getElementById('life-mark')       // ライフの光部分
-let life = 100                                              // ライフ初期値
-lifeBar.style.width = "100%"                                // ライフ初期幅
-
-setInterval(() => {
-life = life + 10;
-if ( life > 100 ) {
-    life = 100
-}
-lifeBar.style.width = life + "%"
-},1500);
-
-function damage(){
-  life = life - 20;
-
-  Player_UI.style.marginTop = "10px";
-  Player_UI.style.marginleft = "10px";
-  Player_UI.style.margintop = "0px";
-  Player_UI.style.marginleft = "0px";
-
-  if ( life <= 0 ){
-  // 算出の結果 0 以下になった場合
-  life = 0
-  // 0.3秒後に光部分を非表示にする
-  setTimeout(function(){
-      lifeMark.style.visibility = 'hidden'
-      Player_Respawn.style.display = "block";
-      life = 100  
-  }, 300)
-  } else {
-  // 算出の結果 100 を超過した場合
-  if ( life > 100 ) {
-      life = 100
-  }
-  // 光部分を表示する
-  lifeMark.style.visibility = 'visible'
-  }
-
-  lifeBar.style.width = life + "%"
-}
-    
-
-
-
 // Handles user-entered messages
 export default class MessageDispatch extends EventTarget {
-  constructor(scene, entryManager, hubChannel, remountUI, mediaSearchStore) {
+  constructor(scene, entryManager, hubChannel, addToPresenceLog, remountUI, mediaSearchStore) {
     super();
     this.scene = scene;
     this.entryManager = entryManager;
     this.hubChannel = hubChannel;
+    this.addToPresenceLog = addToPresenceLog;
     this.remountUI = remountUI;
     this.mediaSearchStore = mediaSearchStore;
-    this.presenceLogEntries = [];
-  }
-
-  addToPresenceLog(entry) {
-    entry.key = Date.now().toString();
-    //var naf_Mine = sessionStorage.getItem('naf-mine'); 
-
-    if (entry.type ==="chat" && entry.body.indexOf("naf") === 0){
-      //if (naf_Mine == entry.body) {
-      damage();
-      console.log(entry.body)
-      //};
-      return
-    };
-
-    this.presenceLogEntries.push(entry);
-    this.remountUI({ presenceLogEntries: this.presenceLogEntries });
-    if (entry.type === "chat" && this.scene.is("loaded")) {
-      this.scene.systems["hubs-systems"].soundEffectsSystem.playSoundOneShot(SOUND_CHAT_MESSAGE);
-    }
-
-    // Fade out and then remove
-    setTimeout(() => {
-      entry.expired = true;
-      this.remountUI({ presenceLogEntries: this.presenceLogEntries });
-
-      setTimeout(() => {
-        this.presenceLogEntries.splice(this.presenceLogEntries.indexOf(entry), 1);
-        this.remountUI({ presenceLogEntries: this.presenceLogEntries });
-      }, 5000);
-    }, 20000);
   }
 
   receive(message) {
@@ -124,8 +45,6 @@ export default class MessageDispatch extends EventTarget {
     uiRoot = uiRoot || document.getElementById("ui-root");
     const isGhost = !entered && uiRoot && uiRoot.firstChild && uiRoot.firstChild.classList.contains("isGhost");
 
-    // TODO: Some of the commands below should be available without requiring
-    //       room entry. For example, audiomode should not require room entry.
     if (!entered && (!isGhost || command === "duck")) {
       this.log(LogMessageType.roomEntryRequired);
       return;
@@ -234,15 +153,8 @@ export default class MessageDispatch extends EventTarget {
         {
           const shouldEnablePositionalAudio = window.APP.store.state.preferences.audioOutputMode === "audio";
           window.APP.store.update({
-            // TODO: This should probably just be a boolean to disable panner node settings
-            // and even if it's not, "audio" is a weird name for the "audioOutputMode" that means
-            // "stereo" / "not panner".
             preferences: { audioOutputMode: shouldEnablePositionalAudio ? "panner" : "audio" }
           });
-          // TODO: The user message here is a little suspicious. We might be ignoring the
-          // user preference (e.g. if panner nodes are broken in safari, then we never create
-          // panner nodes, regardless of user preference.)
-          // Warning: This comment may be out of date when you read it.
           this.log(
             shouldEnablePositionalAudio ? LogMessageType.positionalAudioEnabled : LogMessageType.positionalAudioDisabled
           );
