827a6e7c346b3c832a203d48401e73e70bec2d3b
isseiini <i-kurata@vleap.jp>
2021-11-25 07:53:54 +0900

    9

--- src/message-dispatch.js
+++ src/message-dispatch.js
@@ -11,49 +11,7 @@ import { LogMessageType } from "./react-components/room/ChatSidebar";
 
 let uiRoot;
 
-var Player_UI = document.getElementById("Player-UI")
 
-const lifeBar = document.getElementById('life-bar')         // ライフバー
-const lifeMark = document.getElementById('life-mark')       // ライフの光部分
-let life = 100                                              // ライフ初期値
-lifeBar.style.width = "100%"                                // ライフ初期幅
-
-setInterval(() => {
-life = life + 10;
-if ( life > 100 ) {
-    life = 100
-}
-lifeBar.style.width = life + "%"
-},1500);
-
-function damage(){
-  life = life - 20;
-
-  Player_UI.style.marginTop = "10px";
-  Player_UI.style.marginleft = "10px";
-  Player_UI.style.margintop = "0px";
-  Player_UI.style.marginleft = "0px";
-
-  if ( life <= 0 ){
-  // 算出の結果 0 以下になった場合
-  life = 0
-  // 0.3秒後に光部分を非表示にする
-  setTimeout(function(){
-      lifeMark.style.visibility = 'hidden'
-      Player_Respawn.style.display = "block";
-      life = 100  
-  }, 300)
-  } else {
-  // 算出の結果 100 を超過した場合
-  if ( life > 100 ) {
-      life = 100
-  }
-  // 光部分を表示する
-  lifeMark.style.visibility = 'visible'
-  }
-
-  lifeBar.style.width = life + "%"
-}
     
 
 
