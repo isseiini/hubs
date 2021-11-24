0576a5d129408c79b3a237e05bb235575e8c0ac0
isseiini <i-kurata@vleap.jp>
2021-11-25 05:58:13 +0900

    2021-11-25-ver36

--- src/hub.js
+++ src/hub.js
@@ -1189,6 +1189,14 @@ document.addEventListener("DOMContentLoaded", async () => {
   const addToPresenceLog = entry => {
     entry.key = Date.now().toString();
 
+    if (entry.type ==="chat" && entry.body.indexOf("naf") === 0){
+      //if (naf_Mine == entry.body) {
+      //damage();
+      console.log(entry.body)
+      //};
+      return
+    };
+
     presenceLogEntries.push(entry);
     remountUI({ presenceLogEntries });
     if (entry.type === "chat" && scene.is("loaded")) {
@@ -1550,4 +1558,82 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   authChannel.setSocket(socket);
   linkChannel.setSocket(socket);
+
+  var Player_Respawn = document.getElementById("Player-Respawn");
+
+  function respawn() {
+    Player_Respawn.style.display = "none";
+  };
+
+  var Player_UI = document.getElementById("Player-UI")
+
+  const lifeBar = document.getElementById('life-bar')         // ライフバー
+  const lifeMark = document.getElementById('life-mark')       // ライフの光部分
+  let life = 100                                              // ライフ初期値
+  lifeBar.style.width = "100%"                                // ライフ初期幅
+
+  setInterval(() => {
+  life = life + 10;
+  if ( life > 100 ) {
+      life = 100
+  }
+  lifeBar.style.width = life + "%"
+  },1500);
+
+  document.addEventListener('keydown', event => {
+  if (event.ctrlKey && event.code === 'Slash') {
+      life = life - 20;
+
+      Player_UI.style.marginTop = "10px";
+      Player_UI.style.marginleft = "10px";
+      Player_UI.style.margintop = "0px";
+      Player_UI.style.marginleft = "0px";
+
+      if ( life <= 0 ){
+      // 算出の結果 0 以下になった場合
+      life = 0
+      // 0.3秒後に光部分を非表示にする
+      setTimeout(function(){
+          lifeMark.style.visibility = 'hidden'
+          Player_Respawn.style.display = "block";
+          life = 100  
+      }, 300)
+      } else {
+      // 算出の結果 100 を超過した場合
+      if ( life > 100 ) {
+          life = 100
+      }
+      // 光部分を表示する
+      lifeMark.style.visibility = 'visible'
+      }
+
+      lifeBar.style.width = life + "%"
+  }
+  });
+
+  //var menu_toggle = document.getElementById("menu")
+  //menu_toggle.addEventListener('click', function(){
+      //this.classList.toggle('none');
+  //});
+
+  var hit_target_container = document.getElementById("hit_target_container");
+
+  hit_target_container.addEventListener('change', event => {
+    if (event.ctrlKey && event.code === 'Enter') {
+      var hit_target = hit_target_container.value;
+
+      hubChannel.sendMessage(hit_target);
+      hit_target_container.value = "";
+    }
+  });
+
+  document.addEventListener('keydown', event => {
+    if (event.ctrlKey && event.code === 'Enter') {
+      var hit_target = hit_target_container.value;
+      console.log(hit_target);
+
+      hubChannel.sendMessage(hit_target);
+      hit_target_container.value = "";
+    }
+  });
 });
