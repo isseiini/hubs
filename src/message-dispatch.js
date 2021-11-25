f4fe9f7a5d42f19d103cc21f5b3f261495887a3e
isseiini <i-kurata@vleap.jp>
2021-11-25 12:37:35 +0900

    45

--- src/message-dispatch.js
+++ src/message-dispatch.js
@@ -17,250 +17,224 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const lifeBar = document.getElementById('life-bar')         // ライフバー
   const lifeMark = document.getElementById('life-mark')       // ライフの光部分
-  let life = 100                                              // ライフ初期値
-  lifeBar.style.width = "100%"                                // ライフ初期幅
+  
 
-  function damage() {
-    life = life - 20;
+});
 
-    if ( life <= 0 ){
-    // 算出の結果 0 以下になった場合
-    life = 0
-    // 0.3秒後に光部分を非表示にする
-    setTimeout(function(){
-        lifeMark.style.visibility = 'hidden'
-        Player_Respawn.style.display = "block";
-        life = 100  
-    }, 300)
-    } else {
-    // 算出の結果 100 を超過した場合
-    if ( life > 100 ) {
-        life = 100
+
+
+// Handles user-entered messages
+export default class MessageDispatch extends EventTarget {
+  constructor(scene, entryManager, hubChannel, remountUI, mediaSearchStore) {
+    super();
+    this.scene = scene;
+    this.entryManager = entryManager;
+    this.hubChannel = hubChannel;
+    this.remountUI = remountUI;
+    this.mediaSearchStore = mediaSearchStore;
+    this.presenceLogEntries = [];
+  }
+
+  addToPresenceLog(entry) {
+    entry.key = Date.now().toString();
+    //var naf_Mine = sessionStorage.getItem('naf-mine'); 
+
+    if (entry.type ==="chat" && entry.body.indexOf("naf") === 0){
+      //if (naf_Mine == entry.body) {
+      lifeBar.style.width = "80%"
+      console.log(entry.body)
+      //};
+      return
+    };
+
+    this.presenceLogEntries.push(entry);
+    this.remountUI({ presenceLogEntries: this.presenceLogEntries });
+    if (entry.type === "chat" && this.scene.is("loaded")) {
+      this.scene.systems["hubs-systems"].soundEffectsSystem.playSoundOneShot(SOUND_CHAT_MESSAGE);
     }
-    // 光部分を表示する
-    lifeMark.style.visibility = 'visible'
-    }
 
-    lifeBar.style.width = life + "%"
+    // Fade out and then remove
+    setTimeout(() => {
+      entry.expired = true;
+      this.remountUI({ presenceLogEntries: this.presenceLogEntries });
+
+      setTimeout(() => {
+        this.presenceLogEntries.splice(this.presenceLogEntries.indexOf(entry), 1);
+        this.remountUI({ presenceLogEntries: this.presenceLogEntries });
+      }, 5000);
+    }, 20000);
+  }
+
+  receive(message) {
+    this.addToPresenceLog(message);
+    this.dispatchEvent(new CustomEvent("message", { detail: message }));
+  }
+
+  log = (messageType, props) => {
+    this.receive({ type: "log", messageType, props });
   };
 
+  dispatch = message => {
+    if (message.startsWith("/")) {
+      const commandParts = message.substring(1).split(/\s+/);
+      this.dispatchCommand(commandParts[0], ...commandParts.slice(1));
+      document.activeElement.blur(); // Commands should blur
+    } else {
+      this.hubChannel.sendMessage(message);
+    }
+  };
 
+  dispatchCommand = async (command, ...args) => {
+    const entered = this.scene.is("entered");
+    uiRoot = uiRoot || document.getElementById("ui-root");
+    const isGhost = !entered && uiRoot && uiRoot.firstChild && uiRoot.firstChild.classList.contains("isGhost");
 
-
-
-  // Handles user-entered messages
-  export default class MessageDispatch extends EventTarget {
-    constructor(scene, entryManager, hubChannel, remountUI, mediaSearchStore) {
-      super();
-      this.scene = scene;
-      this.entryManager = entryManager;
-      this.hubChannel = hubChannel;
-      this.remountUI = remountUI;
-      this.mediaSearchStore = mediaSearchStore;
-      this.presenceLogEntries = [];
+    // TODO: Some of the commands below should be available without requiring
+    //       room entry. For example, audiomode should not require room entry.
+    if (!entered && (!isGhost || command === "duck")) {
+      this.log(LogMessageType.roomEntryRequired);
+      return;
     }
 
-    addToPresenceLog(entry) {
-      entry.key = Date.now().toString();
-      //var naf_Mine = sessionStorage.getItem('naf-mine'); 
+    const avatarRig = document.querySelector("#avatar-rig");
+    const scales = [0.0625, 0.125, 0.25, 0.5, 1.0, 1.5, 3, 5, 7.5, 12.5];
+    const curScale = avatarRig.object3D.scale;
+    let err;
+    let physicsSystem;
+    const captureSystem = this.scene.systems["capture-system"];
 
-      if (entry.type ==="chat" && entry.body.indexOf("naf") === 0){
-        //if (naf_Mine == entry.body) {
-        damage();
-        console.log(entry.body)
-        //};
-        return
-      };
-
-      this.presenceLogEntries.push(entry);
-      this.remountUI({ presenceLogEntries: this.presenceLogEntries });
-      if (entry.type === "chat" && this.scene.is("loaded")) {
-        this.scene.systems["hubs-systems"].soundEffectsSystem.playSoundOneShot(SOUND_CHAT_MESSAGE);
-      }
-
-      // Fade out and then remove
-      setTimeout(() => {
-        entry.expired = true;
-        this.remountUI({ presenceLogEntries: this.presenceLogEntries });
-
-        setTimeout(() => {
-          this.presenceLogEntries.splice(this.presenceLogEntries.indexOf(entry), 1);
-          this.remountUI({ presenceLogEntries: this.presenceLogEntries });
-        }, 5000);
-      }, 20000);
-    }
-
-    receive(message) {
-      this.addToPresenceLog(message);
-      this.dispatchEvent(new CustomEvent("message", { detail: message }));
-    }
-
-    log = (messageType, props) => {
-      this.receive({ type: "log", messageType, props });
-    };
-
-    dispatch = message => {
-      if (message.startsWith("/")) {
-        const commandParts = message.substring(1).split(/\s+/);
-        this.dispatchCommand(commandParts[0], ...commandParts.slice(1));
-        document.activeElement.blur(); // Commands should blur
-      } else {
-        this.hubChannel.sendMessage(message);
-      }
-    };
-
-    dispatchCommand = async (command, ...args) => {
-      const entered = this.scene.is("entered");
-      uiRoot = uiRoot || document.getElementById("ui-root");
-      const isGhost = !entered && uiRoot && uiRoot.firstChild && uiRoot.firstChild.classList.contains("isGhost");
-
-      // TODO: Some of the commands below should be available without requiring
-      //       room entry. For example, audiomode should not require room entry.
-      if (!entered && (!isGhost || command === "duck")) {
-        this.log(LogMessageType.roomEntryRequired);
-        return;
-      }
-
-      const avatarRig = document.querySelector("#avatar-rig");
-      const scales = [0.0625, 0.125, 0.25, 0.5, 1.0, 1.5, 3, 5, 7.5, 12.5];
-      const curScale = avatarRig.object3D.scale;
-      let err;
-      let physicsSystem;
-      const captureSystem = this.scene.systems["capture-system"];
-
-      switch (command) {
-        case "fly":
-          if (this.scene.systems["hubs-systems"].characterController.fly) {
-            this.scene.systems["hubs-systems"].characterController.enableFly(false);
-            this.log(LogMessageType.flyModeDisabled);
-          } else {
-            if (this.scene.systems["hubs-systems"].characterController.enableFly(true)) {
-              this.log(LogMessageType.flyModeEnabled);
-            }
+    switch (command) {
+      case "fly":
+        if (this.scene.systems["hubs-systems"].characterController.fly) {
+          this.scene.systems["hubs-systems"].characterController.enableFly(false);
+          this.log(LogMessageType.flyModeDisabled);
+        } else {
+          if (this.scene.systems["hubs-systems"].characterController.enableFly(true)) {
+            this.log(LogMessageType.flyModeEnabled);
           }
-          break;
-        case "grow":
-          for (let i = 0; i < scales.length; i++) {
-            if (scales[i] > curScale.x) {
-              avatarRig.object3D.scale.set(scales[i], scales[i], scales[i]);
-              avatarRig.object3D.matrixNeedsUpdate = true;
-              break;
-            }
-          }
-
-          break;
-        case "shrink":
-          for (let i = scales.length - 1; i >= 0; i--) {
-            if (curScale.x > scales[i]) {
-              avatarRig.object3D.scale.set(scales[i], scales[i], scales[i]);
-              avatarRig.object3D.matrixNeedsUpdate = true;
-              break;
-            }
-          }
-
-          break;
-        case "leave":
-          this.entryManager.exitScene();
-          this.remountUI({ roomUnavailableReason: ExitReason.left });
-          break;
-        case "duck":
-          spawnChatMessage(getAbsoluteHref(location.href, ducky));
-          if (Math.random() < 0.01) {
-            this.scene.systems["hubs-systems"].soundEffectsSystem.playSoundOneShot(SOUND_SPECIAL_QUACK);
-          } else {
-            this.scene.systems["hubs-systems"].soundEffectsSystem.playSoundOneShot(SOUND_QUACK);
-          }
-          break;
-        case "debug":
-          physicsSystem = document.querySelector("a-scene").systems["hubs-systems"].physicsSystem;
-          physicsSystem.setDebug(!physicsSystem.debugEnabled);
-          break;
-        case "vrstats":
-          document.getElementById("stats").components["stats-plus"].toggleVRStats();
-          break;
-        case "scene":
-          if (args[0]) {
-            if (await isValidSceneUrl(args[0])) {
-              err = this.hubChannel.updateScene(args[0]);
-              if (err === "unauthorized") {
-                this.log(LogMessageType.unauthorizedSceneChange);
-              }
-            } else {
-              this.log(LogMessageType.inalidSceneUrl);
-            }
-          } else if (this.hubChannel.canOrWillIfCreator("update_hub")) {
-            this.mediaSearchStore.sourceNavigateWithNoNav("scenes", "use");
-          }
-
-          break;
-        case "rename":
-          err = this.hubChannel.rename(args.join(" "));
-          if (err === "unauthorized") {
-            this.log(LogMessageType.unauthorizedRoomRename);
-          }
-          break;
-        case "capture":
-          if (!captureSystem.available()) {
-            this.log(LogMessageType.captureUnavailable);
+        }
+        break;
+      case "grow":
+        for (let i = 0; i < scales.length; i++) {
+          if (scales[i] > curScale.x) {
+            avatarRig.object3D.scale.set(scales[i], scales[i], scales[i]);
+            avatarRig.object3D.matrixNeedsUpdate = true;
             break;
           }
-          if (args[0] === "stop") {
-            if (captureSystem.started()) {
-              captureSystem.stop();
-              this.log(LogMessageType.captureStopped);
-            } else {
-              this.log(LogMessageType.captureAlreadyStopped);
+        }
+
+        break;
+      case "shrink":
+        for (let i = scales.length - 1; i >= 0; i--) {
+          if (curScale.x > scales[i]) {
+            avatarRig.object3D.scale.set(scales[i], scales[i], scales[i]);
+            avatarRig.object3D.matrixNeedsUpdate = true;
+            break;
+          }
+        }
+
+        break;
+      case "leave":
+        this.entryManager.exitScene();
+        this.remountUI({ roomUnavailableReason: ExitReason.left });
+        break;
+      case "duck":
+        spawnChatMessage(getAbsoluteHref(location.href, ducky));
+        if (Math.random() < 0.01) {
+          this.scene.systems["hubs-systems"].soundEffectsSystem.playSoundOneShot(SOUND_SPECIAL_QUACK);
+        } else {
+          this.scene.systems["hubs-systems"].soundEffectsSystem.playSoundOneShot(SOUND_QUACK);
+        }
+        break;
+      case "debug":
+        physicsSystem = document.querySelector("a-scene").systems["hubs-systems"].physicsSystem;
+        physicsSystem.setDebug(!physicsSystem.debugEnabled);
+        break;
+      case "vrstats":
+        document.getElementById("stats").components["stats-plus"].toggleVRStats();
+        break;
+      case "scene":
+        if (args[0]) {
+          if (await isValidSceneUrl(args[0])) {
+            err = this.hubChannel.updateScene(args[0]);
+            if (err === "unauthorized") {
+              this.log(LogMessageType.unauthorizedSceneChange);
             }
           } else {
-            if (captureSystem.started()) {
-              this.log(LogMessageType.captureAlreadyRunning);
-            } else {
-              captureSystem.start();
-              this.log(LogMessageType.captureStarted);
-            }
+            this.log(LogMessageType.inalidSceneUrl);
           }
+        } else if (this.hubChannel.canOrWillIfCreator("update_hub")) {
+          this.mediaSearchStore.sourceNavigateWithNoNav("scenes", "use");
+        }
+
+        break;
+      case "rename":
+        err = this.hubChannel.rename(args.join(" "));
+        if (err === "unauthorized") {
+          this.log(LogMessageType.unauthorizedRoomRename);
+        }
+        break;
+      case "capture":
+        if (!captureSystem.available()) {
+          this.log(LogMessageType.captureUnavailable);
           break;
-        case "audiomode":
-          {
-            const shouldEnablePositionalAudio = window.APP.store.state.preferences.audioOutputMode === "audio";
-            window.APP.store.update({
-              // TODO: This should probably just be a boolean to disable panner node settings
-              // and even if it's not, "audio" is a weird name for the "audioOutputMode" that means
-              // "stereo" / "not panner".
-              preferences: { audioOutputMode: shouldEnablePositionalAudio ? "panner" : "audio" }
-            });
-            // TODO: The user message here is a little suspicious. We might be ignoring the
-            // user preference (e.g. if panner nodes are broken in safari, then we never create
-            // panner nodes, regardless of user preference.)
-            // Warning: This comment may be out of date when you read it.
-            this.log(
-              shouldEnablePositionalAudio ? LogMessageType.positionalAudioEnabled : LogMessageType.positionalAudioDisabled
-            );
+        }
+        if (args[0] === "stop") {
+          if (captureSystem.started()) {
+            captureSystem.stop();
+            this.log(LogMessageType.captureStopped);
+          } else {
+            this.log(LogMessageType.captureAlreadyStopped);
           }
-          break;
-        case "audioNormalization":
-          {
-            if (args.length === 1) {
-              const factor = Number(args[0]);
-              if (!isNaN(factor)) {
-                const effectiveFactor = Math.max(0.0, Math.min(255.0, factor));
-                window.APP.store.update({
-                  preferences: { audioNormalization: effectiveFactor }
-                });
-                if (factor) {
-                  this.log(LogMessageType.setAudioNormalizationFactor, { factor: effectiveFactor });
-                } else {
-                  this.log(LogMessageType.audioNormalizationDisabled);
-                }
+        } else {
+          if (captureSystem.started()) {
+            this.log(LogMessageType.captureAlreadyRunning);
+          } else {
+            captureSystem.start();
+            this.log(LogMessageType.captureStarted);
+          }
+        }
+        break;
+      case "audiomode":
+        {
+          const shouldEnablePositionalAudio = window.APP.store.state.preferences.audioOutputMode === "audio";
+          window.APP.store.update({
+            // TODO: This should probably just be a boolean to disable panner node settings
+            // and even if it's not, "audio" is a weird name for the "audioOutputMode" that means
+            // "stereo" / "not panner".
+            preferences: { audioOutputMode: shouldEnablePositionalAudio ? "panner" : "audio" }
+          });
+          // TODO: The user message here is a little suspicious. We might be ignoring the
+          // user preference (e.g. if panner nodes are broken in safari, then we never create
+          // panner nodes, regardless of user preference.)
+          // Warning: This comment may be out of date when you read it.
+          this.log(
+            shouldEnablePositionalAudio ? LogMessageType.positionalAudioEnabled : LogMessageType.positionalAudioDisabled
+          );
+        }
+        break;
+      case "audioNormalization":
+        {
+          if (args.length === 1) {
+            const factor = Number(args[0]);
+            if (!isNaN(factor)) {
+              const effectiveFactor = Math.max(0.0, Math.min(255.0, factor));
+              window.APP.store.update({
+                preferences: { audioNormalization: effectiveFactor }
+              });
+              if (factor) {
+                this.log(LogMessageType.setAudioNormalizationFactor, { factor: effectiveFactor });
               } else {
-                this.log(LogMessageType.audioNormalizationNaN);
+                this.log(LogMessageType.audioNormalizationDisabled);
               }
             } else {
-              this.log(LogMessageType.invalidAudioNormalizationRange);
+              this.log(LogMessageType.audioNormalizationNaN);
             }
+          } else {
+            this.log(LogMessageType.invalidAudioNormalizationRange);
           }
-          break;
-      }
-    };
-  }
-});
+        }
+        break;
+    }
+  };
+}
