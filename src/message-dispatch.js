c4aa80cd12fdc013b94ca19a3d3a5b534733ec73
isseiini <i-kurata@vleap.jp>
2021-11-26 08:32:22 +0900

    2021-11-26-ver01

--- src/message-dispatch.js
+++ src/message-dispatch.js
@@ -35,13 +35,47 @@ export default class MessageDispatch extends EventTarget {
     this.presenceLogEntries = [];
   }
 
+  damage() {
+    var Player_UI = document.getElementById("Player-UI")
+
+    const lifeBar = document.getElementById('life-bar')         // ライフバー
+    const lifeMark = document.getElementById('life-mark')       // ライフの光部分
+    let life = 100                                              // ライフ初期値
+    lifeBar.style.width = "100%"                                // ライフ初期幅
+
+    document.addEventListener('keydown', event => {
+      life = life - 20;
+
+      if ( life <= 0 ){
+      // 算出の結果 0 以下になった場合
+      life = 0
+      // 0.3秒後に光部分を非表示にする
+      setTimeout(function(){
+          lifeMark.style.visibility = 'hidden'
+          Player_Respawn.style.display = "block";
+          life = 100  
+      }, 300)
+      } else {
+      // 算出の結果 100 を超過した場合
+      if ( life > 100 ) {
+          life = 100
+      }
+      // 光部分を表示する
+      lifeMark.style.visibility = 'visible'
+      }
+
+      lifeBar.style.width = life + "%"
+      
+    });
+  }
+
   addToPresenceLog(entry) {
     entry.key = Date.now().toString();
     //var naf_Mine = sessionStorage.getItem('naf-mine'); 
 
     if (entry.type ==="chat" && entry.body.indexOf("naf") === 0){
       //if (naf_Mine == entry.body) {
-      //lifeBar.style.width = "80%"
+      this.damage();
       console.log(entry.body)
       //};
       return
