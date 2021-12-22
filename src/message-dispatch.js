c90353635d562eb54952c8c01bec54cb16c4caf9
isseiini <i-kurata@vleap.jp>
2021-12-22 17:40:43 +0900

    2021-12-22-ver84

--- src/message-dispatch.js
+++ src/message-dispatch.js
@@ -81,7 +81,7 @@ export default class MessageDispatch extends EventTarget {
           console.log(err);
         }else{
           if(data.Item.RedPoints >= 10) {
-            this.dispatch("_Win_Red");
+            dispatch("_Win_Red");
           }
         }
       });
