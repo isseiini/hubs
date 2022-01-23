49d5010a73f2a47a21f0177f349c6ef9beea75c7
isseiini <i-kurata@vleap.jp>
2022-01-24 06:27:02 +0900

    ver1.16

--- src/hub.js
+++ src/hub.js
@@ -1817,6 +1817,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.documentElement.style.setProperty('--display6', 'none');
   });
 
+  var AWS = require("aws-sdk");
+
   AWS.config.region = 'ap-northeast-1'; 
   AWS.config.credentials = new AWS.CognitoIdentityCredentials({
       IdentityPoolId: 'ap-northeast-1:1a5b9f55-2ccb-494f-964f-6fda4d7f9eda',
