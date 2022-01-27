255bd8917959df62ae1c326bf45a97ef0c3d61d2
isseiini <i-kurata@vleap.jp>
2022-01-27 13:36:49 +0900

    ver1.39

--- src/hub.js
+++ src/hub.js
@@ -969,23 +969,6 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
   scene.addEventListener("leave_room_requested", () => {
-    text_chat_data.count();
-    console.log(text_chat_data);
-    var params = {
-      TableName: 'Communication',
-      Item:{
-        playID: "dsagfawg",
-        text_chat: text_chat_data
-      }
-    };
-    docClient.put(params, function(err, data){
-      if(err){
-        console.log(err);
-      }else{
-        console.log(success);
-      }
-    });
-
     entryManager.exitScene();
     remountUI({ roomUnavailableReason: ExitReason.left });
   });
@@ -1100,6 +1083,23 @@ document.addEventListener("DOMContentLoaded", async () => {
       entryManager.exitScene();
       remountUI({ roomUnavailableReason: ExitReason.disconnected });
     }
+
+    text_chat_data.count();
+    console.log(text_chat_data);
+    var params = {
+      TableName: 'Communication',
+      Item:{
+        playID: "dsagfawg",
+        text_chat: text_chat_data
+      }
+    };
+    docClient.put(params, function(err, data){
+      if(err){
+        console.log(err);
+      }else{
+        console.log(success);
+      }
+    });
   });
 
   // Reticulum global channel
