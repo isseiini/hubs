f205b56e5e7aaa2005d6e4c96b08496b21920cf9
isseiini <i-kurata@vleap.jp>
2022-02-02 09:32:07 +0900

    ver1.79

--- src/hub.js
+++ src/hub.js
@@ -712,12 +712,16 @@ document.addEventListener("DOMContentLoaded", async () => {
   Player_UI.style.display = "none";
   const toolbar_under = document.getElementById("toolbar_under");
   toolbar_under.style.display = "none";
-  const menu = document.getElementById("menu");
-  menu.addEventListener("click", function(){
-    toolbar_under.style.display = "block";
-  });
   const contentmenu_top = document.getElementById("contentmenu_top");
   contentmenu_top.style.display = "none";
+  const Player_tips = document.getElementById("Player_tips");
+  Player_tips.style.display = "none";
+  const general_menu = document.getElementById("general_menu");
+  general_menu.addEventListener("click", function(){
+    toolbar_under.style.display = "block";
+    Player_tips.style.display = "block";
+  });
+  
 
   if (room_name == "kooky-passionate-safari") {
     document.getElementById("hex-background").style.display = "none";
