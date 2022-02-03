422784bacaf6300c645b4e9c389dfbaaebd5fd23
isseiini <i-kurata@vleap.jp>
2022-02-04 05:51:17 +0900

    ver2.22

--- src/components/media-loader.js
+++ src/components/media-loader.js
@@ -204,10 +204,6 @@ AFRAME.registerComponent("media-loader", {
 
       this.loadingClip = this.loaderMixer.clipAction(mesh.animations[0]);
 
-      console.log(mesh);
-      console.log(this.el);
-      console.log(this.loaderMixer);
-      console.log(this.loadingClip);
       this.loadingScaleClip = this.loaderMixer.clipAction(
         new THREE.AnimationClip(null, 1000, [
           new THREE.VectorKeyframeTrack(".scale", [0, 0.2], [0, 0, 0, mesh.scale.x, mesh.scale.y, mesh.scale.z])
