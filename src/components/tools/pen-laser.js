9e1e3cbd8a63bf51b6bc6e2c3253080221cee0a5
isseiini <i-kurata@vleap.jp>
2022-02-15 07:08:02 +0900

    ver3.65

--- src/components/tools/pen-laser.js
+++ src/components/tools/pen-laser.js
@@ -30,7 +30,7 @@ waitForDOMContentLoaded().then(() => {
 AFRAME.registerComponent("pen-laser", {
   schema: {
     color: { type: "color", default: "#FF0033" },
-    laserVisible: { default: false },
+    laserVisible: { default: true },
     laserInHand: { default: false },
     laserOrigin: { default: { x: 0, y: 0, z: 0 } },
     remoteLaserOrigin: { default: { x: 0, y: 0, z: 0 } },
