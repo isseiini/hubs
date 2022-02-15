a3b6dc553a1ead7673a3935b2406be517f29be6b
isseiini <i-kurata@vleap.jp>
2022-02-16 04:53:43 +0900

    ver4.11

--- src/components/tools/pen-laser.js
+++ src/components/tools/pen-laser.js
@@ -159,8 +159,9 @@ AFRAME.registerComponent("pen-laser", {
       if (laserVisible) {
         //origin.y += 1;
         //origin.z += -0.5;
-        origin.y = origin.y + 0.25;
-        origin.z = origin.z - 0.20;
+        origin.x = origin.x - 0.1;
+        origin.y = origin.y + 0.3;
+        origin.z = origin.z - 0.3;
         this.laser.position.copy(origin);
         console.log("origin:" + origin.x + "," + origin.y + "," + origin.z)
         this.laser.lookAt(target);
