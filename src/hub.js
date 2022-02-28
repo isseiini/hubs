894e03520b1f518698ce6be69963d47b9eaf8809
isseiini <i-kurata@vleap.jp>
2022-02-28 14:40:59 +0900

    ver6.93

--- src/hub.js
+++ src/hub.js
@@ -578,7 +578,7 @@ class myCognitouserclass extends CognitoUser{
 	}
 
   cacheTokens() {
-    this.signInUserSession = this.getcachedsession
+    this.signInUserSession = this.getCognitoUserSession(authenticationResult)
     const get_idToken = this.signInUserSession.getIdToken().getJwtToken();
     const get_accessToken = this.signInUserSession.getAccessToken().getJwtToken()
     const get_refreshToken = this.signInUserSession.getRefreshToken().getToken()
