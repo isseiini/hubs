2248a13a561e42b41e331724ad3fd355d8669432
isseiini <i-kurata@vleap.jp>
2022-03-01 10:39:12 +0900

    ver7.27

--- src/hub.js
+++ src/hub.js
@@ -3533,7 +3533,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   });
 
   document.addEventListener('keyup', event => {
-    if (e.keyCode === 13 && e.altKey=== true) {
+    if (event.shiftKey === true && event.altKey=== true) {
       var cognitoUser_me2 = userPool.exgetCurrentUser(); 
       cognitoUser_me2.exgetSession((err, session) => {
         if (err) {
