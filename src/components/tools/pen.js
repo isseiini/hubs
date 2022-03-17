017492cba1ef1864b600a646a7246bd1ecae13b1
isseiini <i-kurata@vleap.jp>
2022-03-17 17:51:25 +0900

    ver12.50

--- src/components/tools/pen.js
+++ src/components/tools/pen.js
@@ -419,7 +419,7 @@ AFRAME.registerComponent("pen", {
           const aircanon_index = "." + myname + "_aircanon";
           //const AirCanonAction = document.querySelector(aircanon_index);//document.getElementById("pen");
           let AirCanonAction = document.querySelectorAll(".aircanon");
-          AirCanonAction = AirCanonAction[AirCanonAction.length - 1];
+          AirCanonAction = AirCanonAction[0];
           AirCanonAction.setAttribute("aircanon-animation", {action: "true"});
           setTimeout(() => {
             AirCanonAction.setAttribute("aircanon-animation", {action: "false"});
