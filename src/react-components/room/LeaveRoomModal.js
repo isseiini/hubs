109532406a3dee8d55fa7a2f8d9844e29c7caf03
isseiini <i-kurata@vleap.jp>
2022-03-19 12:42:52 +0900

    ver13.50

--- src/react-components/room/LeaveRoomModal.js
+++ src/react-components/room/LeaveRoomModal.js
@@ -64,5 +64,5 @@ LeaveRoomModal.propTypes = {
   reason: PropTypes.string,
   destinationUrl: PropTypes.string,
   onClose: PropTypes.func,
-  onLeave: PropTypes.func
+  onLeave: PropTypes.checkPropTypes
 };
