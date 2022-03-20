5104020a4f47ca1535cddc334be62358c4821127
isseiini <i-kurata@vleap.jp>
2022-03-20 11:47:45 +0900

    ver13.86

--- src/react-components/room/LeaveRoomModal.js
+++ src/react-components/room/LeaveRoomModal.js
@@ -106,67 +106,6 @@ export function LeaveRoomModal({ reason, destinationUrl, onClose }) {
     });
   }
 
-  document.getElementById("leave_button_confirmed").addEventListener("click", function() {
-    const arr1 = [
-      "adorable-keen-zone",
-      "posh-courteous-plane",
-      "curly-wicked-conclave",
-      "clever-powerful-gala",
-      "kooky-passionate-safari"
-    ];
-    const arr2 = [
-      "conscious-tricky-camp",
-      "impressive-easygoing-commons",
-      "fine-zigzag-exploration",
-      "wee-likable-commons",
-      "envious-shiny-vacation"
-    ];
-    const arr3 = [
-      "devoted-healthy-gala",
-      "petty-handsome-plaza",
-      "real-qualified-spot",
-      "absolute-pertinent-convention",
-      "neat-striking-spot"
-    ];
-    const arr4 = [
-      "celebrated-calm-rendezvous",
-      "lasting-spiffy-camp",
-      "leafy-expert-dominion",
-      "melodic-courageous-picnic",
-      "plump-cheerful-plane"
-    ];
-    var current_url = (location.protocol + "//" + location.hostname + location.pathname).split("/");
-
-    var room_name = string(current_url[current_url.length - 1]);
-    if (arr1.indexof(room_name) !== -1 || arr3.indexof(room_name) !== -1) {
-      var table = "Matching-table";
-    } else if (arr2.indexof(room_name) !== -1 || arr4.indexof(room_name) !== -1) {
-      var table = "Sightseeing-table";
-    }
-    var match = {
-      TableName: table,
-      Key: {
-        //更新したい項目をプライマリキー(及びソートキー)によって１つ指定
-        URL: this.room_name
-      },
-      ExpressionAttributeNames: {
-        "#S": "Sum"
-      },
-      ExpressionAttributeValues: {
-        ":add": 1
-      },
-      UpdateExpression: "SET #S = #S - :add"
-    };
-    docClient.update(match, function(err, data2) {
-      if (err) {
-        console.log("error");
-      } else {
-        console.log("success");
-        location.href = "/";
-      }
-    });
-  });
-
   return (
     <Modal
       title={<FormattedMessage id="leave-room-modal.title" defaultMessage="トップページに戻ります" />}
