f8b914b25f25586ae6ff80447b46b97ccc89b256
isseiini <i-kurata@vleap.jp>
2022-03-21 09:59:57 +0900

    ver14.40

--- src/react-components/room/useMicrophone.js
+++ src/react-components/room/useMicrophone.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+/*import { useState, useEffect, useRef, useCallback } from "react";
 import MovingAverage from "moving-average";
 
 export function useMicrophone(scene, updateRate = 50) {
@@ -55,4 +55,68 @@ export function useMicrophone(scene, updateRate = 50) {
   );
 
   return { isMuted, volume, toggleMute };
+}*/
+
+import { useState, useEffect, useCallback } from "react";
+import { MediaDevices, MediaDevicesEvents } from "../../utils/media-devices-utils";
+
+export function useMicrophone(scene) {
+  const mediaDevicesManager = window.APP.mediaDevicesManager;
+  const [selectedMicDeviceId, setSelectedMicDeviceId] = useState(mediaDevicesManager.selectedMicDeviceId);
+  const [micDevices, setMicDevices] = useState(mediaDevicesManager.micDevices);
+
+  useEffect(
+    () => {
+      const onMicEnabled = () => {
+        setSelectedMicDeviceId(mediaDevicesManager.selectedMicDeviceId);
+        setMicDevices(mediaDevicesManager.micDevices);
+      };
+      const onMicDisabled = () => {
+        setSelectedMicDeviceId(mediaDevicesManager.selectedMicDeviceId);
+        setMicDevices(mediaDevicesManager.micDevices);
+      };
+      scene.addEventListener(MediaDevicesEvents.MIC_SHARE_ENDED, onMicDisabled);
+      scene.addEventListener(MediaDevicesEvents.MIC_SHARE_STARTED, onMicEnabled);
+
+      const onPermissionsChanged = ({ mediaDevice }) => {
+        if (mediaDevice === MediaDevices.MICROPHONE) {
+          setSelectedMicDeviceId(mediaDevicesManager.selectedMicDeviceId);
+          setMicDevices(mediaDevicesManager.micDevices);
+        }
+      };
+      mediaDevicesManager.on(MediaDevicesEvents.PERMISSIONS_STATUS_CHANGED, onPermissionsChanged);
+
+      const onDeviceChange = () => {
+        setSelectedMicDeviceId(mediaDevicesManager.selectedMicDeviceId);
+        setMicDevices(mediaDevicesManager.micDevices);
+      };
+      mediaDevicesManager.on(MediaDevicesEvents.DEVICE_CHANGE, onDeviceChange);
+
+      setSelectedMicDeviceId(mediaDevicesManager.selectedMicDeviceId);
+      setMicDevices(mediaDevicesManager.micDevices);
+
+      return () => {
+        scene.removeEventListener(MediaDevicesEvents.MIC_SHARE_ENDED, onMicDisabled);
+        scene.removeEventListener(MediaDevicesEvents.MIC_SHARE_STARTED, onMicEnabled);
+        mediaDevicesManager.off(MediaDevicesEvents.PERMISSIONS_STATUS_CHANGED, onPermissionsChanged);
+        mediaDevicesManager.off(MediaDevicesEvents.DEVICE_CHANGE, onDeviceChange);
+      };
+    },
+    [setSelectedMicDeviceId, setMicDevices, scene, mediaDevicesManager]
+  );
+
+  const micDeviceChanged = useCallback(
+    deviceId => {
+      setSelectedMicDeviceId(deviceId);
+      setMicDevices(mediaDevicesManager.micDevices);
+      mediaDevicesManager.startMicShare({ deviceId });
+    },
+    [mediaDevicesManager]
+  );
+
+  return {
+    micDeviceChanged,
+    selectedMicDeviceId,
+    micDevices
+  };
 }
