ba9c381fde71e025bc03f6e55260fd71ab9b2e47
isseiini <i-kurata@vleap.jp>
2022-03-21 09:31:37 +0900

    ver14.37

--- src/react-components/ui-root.js
+++ src/react-components/ui-root.js
@@ -621,6 +621,7 @@ class UIRoot extends Component {
 
     // Push the new history state before going into VR, otherwise menu button will take us back
     clearHistoryState(this.props.history);
+    console.log(this.props.store.state.preferences["muteMicOnEntry"]);
 
     const muteOnEntry = this.props.store.state.preferences["muteMicOnEntry"] || false;
     /*this.props.store.update({
