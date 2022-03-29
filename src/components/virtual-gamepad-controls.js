f6d2f5debd8ebbcc6fea0ff8863a16bd5e47eceb
isseiini <i-kurata@vleap.jp>
2022-03-30 06:25:06 +0900

    ver16.45

--- src/components/virtual-gamepad-controls.js
+++ src/components/virtual-gamepad-controls.js
@@ -161,7 +161,7 @@ AFRAME.registerComponent("virtual-gamepad-controls", {
   onMoveJoystickChanged(event, joystick) {
     if (window.APP.preferenceScreenIsVisible) return;
     const angle = joystick.angle.radian;
-    const force = joystick.force < 2 ? joystick.force : 2;
+    const force = joystick.force < 1.5 ? joystick.force : 1.5;
     this.displacement.set(Math.cos(angle), 0, Math.sin(angle)).multiplyScalar(force * 1.85); //change points
     this.moving = true;
   },
