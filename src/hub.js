7cfbb1d61db28b435a5340eb3688156e9f82d1a4
isseiini <i-kurata@vleap.jp>
2022-03-29 09:53:48 +0900

    ver16.14

--- src/hub.js
+++ src/hub.js
@@ -1214,7 +1214,7 @@ export function Get_Coupon(number) {
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-  var isOnIOS = navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i);
+  /*var isOnIOS = navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i);
   if (isOnIOS) {
     window.addEventListener("pagehide", function(event) {
       if (close_flag !== true) {
@@ -1258,7 +1258,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
     event.returnValue = "";
   });
-  /*window.addEventListener("beforeunload", function(event) {
+  window.addEventListener("beforeunload", function(event) {
     
   });*/
 
