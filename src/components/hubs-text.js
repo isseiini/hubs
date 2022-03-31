66d8f37591c165b1cae9326ba6567927aadc0553
isseiini <i-kurata@vleap.jp>
2022-04-01 00:45:09 +0900

    ver16.95

--- src/components/hubs-text.js
+++ src/components/hubs-text.js
@@ -32,65 +32,6 @@ function computeWidth(wrapPixels, wrapCount, widthFactor) {
   return wrapPixels || (0.5 + wrapCount) * widthFactor;
 }
 
-/**
- * Compute default font width factor to use.
- */
-function computeFontWidthFactor(font) {
-  let sum = 0;
-  let digitsum = 0;
-  let digits = 0;
-  for (const ch of font.chars) {
-    sum += ch.xadvance;
-    if (ch.id >= 48 && ch.id <= 57) {
-      digits++;
-      digitsum += ch.xadvance;
-    }
-  }
-  return digits ? digitsum / digits : sum / font.chars.length;
-}
-
-function loadFont(src) {
-  return fetch(src)
-    .then(resp => resp.json())
-    .then(font => {
-      // Fix negative Y offsets for Roboto MSDF font from tool. Experimentally determined.
-      if (src.indexOf("/Roboto-msdf.json") >= 0) {
-        for (const ch of font.chars) {
-          ch.yoffset += 30;
-        }
-      }
-      font.widthFactor = computeFontWidthFactor(font);
-      return font;
-    });
-}
-
-function loadTexture(src) {
-  return new Promise((resolve, reject) => {
-    new THREE.ImageLoader().load(
-      src,
-      image => {
-        const texture = new THREE.Texture();
-        texture.image = image;
-        texture.needsUpdate = true;
-        texture.anisotropy = MAX_ANISOTROPY;
-        resolve(texture);
-      },
-      undefined,
-      err => {
-        console.error("Error loading font image", src, err);
-        reject(null);
-      }
-    );
-  });
-}
-
-const FONTS = {
-  roboto: {
-    json: loadFont("https://cdn.aframe.io/fonts/Roboto-msdf.json"),
-    texture: loadTexture("https://cdn.aframe.io/fonts/Roboto-msdf.png")
-  }
-};
-
 AFRAME.registerComponent("text", {
   multiple: true,
   schema: {
@@ -146,17 +87,6 @@ AFRAME.registerComponent("text", {
     this.shaderObject.update(shaderData);
     this.shaderObject.material.transparent = shaderData.transparent; // Apparently, was not set on `init` nor `update`.
     this.shaderObject.material.side = shaderData.side;
-
-    // New font. `updateFont` will later change data and layout.
-    if (oldData.font !== data.font) {
-      this.updateFont().catch(err => console.error(err));
-    }
-
-    // Not a new font, but we need to accomodate other changes with the existing font.
-    if (this.currentFont != null) {
-      this.updateGeometry(this.geometry, this.currentFont);
-      this.updateLayout(this.currentFont);
-    }
   },
 
   /**
@@ -181,125 +111,6 @@ AFRAME.registerComponent("text", {
       shaderData.transparent = data.transparent;
       shaderData.negate = data.negate;
       return shaderData;
-    };
-  })(),
-
-  /**
-   * Load font for geometry, load font image for material, and apply.
-   */
-  updateFont: function() {
-    const data = this.data;
-    const geometry = this.geometry;
-
-    // Make invisible during font swap.
-    this.mesh.visible = true; //false;
-    const fetchFontData = Promise.all([FONTS[data.font].json, FONTS[data.font].texture]);
-    return fetchFontData.then(([font, texture]) => {
-      if (font.pages.length !== 1) {
-        throw new Error("Currently only single-page bitmap fonts are supported.");
-      }
-      this.currentFont = font;
-      this.updateGeometry(geometry, font);
-      this.shaderObject.update({ map: texture });
-      this.updateLayout(font);
-      this.mesh.visible = true;
-    });
-  },
-
-  /**
-   * Update layout with anchor, alignment, baseline, and considering any meshes.
-   */
-  updateLayout: function(font) {
-    const el = this.el;
-    const data = this.data;
-    const geometry = this.geometry;
-    const mesh = this.mesh;
-
-    if (!geometry.layout) {
-      return;
-    }
-
-    // Determine width to use (defined width, geometry's width, or default width).
-    const geometryComponent = el.getAttribute("geometry");
-    const width = data.width || (geometryComponent && geometryComponent.width) || DEFAULT_WIDTH;
-
-    // Determine wrap pixel count. Either specified or by experimental fudge factor.
-    // Note that experimental factor will never be correct for variable width fonts.
-    const textRenderWidth = computeWidth(data.wrapPixels, data.wrapCount, font.widthFactor);
-    const textScale = width / textRenderWidth;
-
-    // Determine height to use.
-    const layout = geometry.layout;
-    const height = textScale * (layout.height + layout.descender);
-
-    // Update geometry dimensions to match text layout if width and height are set to 0.
-    // For example, scales a plane to fit text.
-    if (geometryComponent && geometryComponent.primitive === "plane") {
-      if (!geometryComponent.width) {
-        el.setAttribute("geometry", "width", width);
-      }
-      if (!geometryComponent.height) {
-        el.setAttribute("geometry", "height", height);
-      }
-    }
-
-    // Calculate X position to anchor text left, center, or right.
-    const anchor = data.anchor === "align" ? data.align : data.anchor;
-    let x, y;
-    if (anchor === "left") {
-      x = 0;
-    } else if (anchor === "right") {
-      x = -1 * layout.width;
-    } else if (anchor === "center") {
-      x = (-1 * layout.width) / 2;
-    } else {
-      throw new TypeError("Invalid text.anchor property value", anchor);
-    }
-
-    // Calculate Y position to anchor text top, center, or bottom.
-    const baseline = data.baseline;
-    if (baseline === "bottom") {
-      y = 0;
-    } else if (baseline === "top") {
-      y = -1 * layout.height + layout.ascender;
-    } else if (baseline === "center") {
-      y = (-1 * layout.height) / 2;
-    } else {
-      throw new TypeError("Invalid text.baseline property value", baseline);
-    }
-
-    // Position and scale mesh to apply layout.
-    mesh.position.x = x * textScale + data.xOffset;
-    mesh.position.y = y * textScale;
-    // Place text slightly in front to avoid Z-fighting.
-    mesh.position.z = data.zOffset;
-    mesh.scale.set(textScale, -1 * textScale, textScale);
-    mesh.matrixNeedsUpdate = true;
-  },
-
-  /**
-   * Update the text geometry using `three-bmfont-text.update`.
-   */
-  updateGeometry: (function() {
-    const geometryUpdateBase = {};
-    const geometryUpdateData = {};
-    const newLineRegex = /\\n/g;
-    const tabRegex = /\\t/g;
-
-    return function(geometry, font) {
-      const data = this.data;
-
-      geometryUpdateData.font = font;
-      geometryUpdateData.lineHeight =
-        data.lineHeight && isFinite(data.lineHeight) ? data.lineHeight : font.common.lineHeight;
-      geometryUpdateData.text = data.value
-        .toString()
-        .replace(newLineRegex, "\n")
-        .replace(tabRegex, "\t");
-      geometryUpdateData.width = computeWidth(data.wrapPixels, data.wrapCount, font.widthFactor);
-      geometry.update(Object.assign(geometryUpdateBase, data, geometryUpdateData));
-      geometry.boundingBox = null;
-      geometry.boundingSphere = null;
     };
   })()
 });
