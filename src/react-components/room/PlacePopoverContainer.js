a1392324badcd8324e3cb1a68ee669a3232505ea
isseiini <i-kurata@vleap.jp>
2022-04-01 16:14:41 +0900

    ver17.72

--- src/react-components/room/PlacePopoverContainer.js
+++ src/react-components/room/PlacePopoverContainer.js
@@ -113,7 +113,7 @@ export function PlacePopoverContainer({ scene, mediaSearchStore, showNonHistorie
     [hubChannel, mediaSearchStore, showNonHistoriedDialog, scene]
   );
 
-  return <PlacePopoverButton items={items} />;
+  return null; //<PlacePopoverButton items={items} />;
 }
 
 PlacePopoverContainer.propTypes = {
