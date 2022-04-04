ead239f2290a432fdd3824fcf010b1c1ea518c8b
isseiini <i-kurata@vleap.jp>
2022-04-04 17:24:51 +0900

    ver18.64

--- src/hub.js
+++ src/hub.js
@@ -1213,7 +1213,7 @@ export function Get_Coupon(number) {
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-  AFRAME.scenes[0].renderer.toneMapping = THREE.ACESfilmicToneMapping();
+  AFRAME.scenes[0].renderer.toneMapping = THREE.LinearMapping();
   AFRAME.scenes[0].renderer.toneMappingExposure = 2;
   AFRAME.scenes[0].object3D.traverse(o => {
     if (o.material) o.material.needsUpdate = true;
