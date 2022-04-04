34cf5e0b4a807988c5058ca8da0c15b9a5aeb94d
isseiini <i-kurata@vleap.jp>
2022-04-05 06:19:22 +0900

    ver18.88

--- src/hub.js
+++ src/hub.js
@@ -679,7 +679,9 @@ function setupLobbyCamera() {
   camera.removeAttribute("scene-preview-camera");
   camera.setAttribute("scene-preview-camera", "positionOnly: true; duration: 60");
 
-  AFRAME.scenes[0].renderer.outputEncoding = THREE.LinearEncoding;
+  AFRAME.scenes[0].renderer.outputEncoding = THREE.sRGBEncoding;
+  AFRAME.scenes[0].renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
   AFRAME.scenes[0].renderer.toneMappingExposure = 1.5;
   AFRAME.scenes[0].renderer.GammaOutput = true;
   AFRAME.scenes[0].renderer.GammaFactor = 2.2;
