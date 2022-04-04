69e58a04a7048c43939a43e963904141345e5867
isseiini <i-kurata@vleap.jp>
2022-04-05 06:01:52 +0900

    ver18.86

--- src/hub.js
+++ src/hub.js
@@ -681,8 +681,7 @@ function setupLobbyCamera() {
 
   AFRAME.scenes[0].renderer.outputEncoding = THREE.LinearEncoding;
   AFRAME.scenes[0].renderer.toneMappingExposure = 1.5;
-  AFRAME.scenes[0].renderer.GammaOutput = true;
-  AFRAME.scenes[0].renderer.GammaFactor = 1.0;
+  AFRAME.scenes[0].renderer.outputEncoding = THREE.sRGBEncoding;
   AFRAME.scenes[0].object3D.traverse(o => {
     if (o.material) o.material.needsUpdate = true;
   });
